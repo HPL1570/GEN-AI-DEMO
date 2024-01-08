1ff36514e98a35c5d5ba0a48b0b5cc0706ce1c8f
Patcha Hemanth Lakshman <137917960+HPL1570@users.noreply.github.com>
2024-01-08 12:15:58 +0530

    Update MYAPP.py

--- MYAPP.py
+++ MYAPP.py
@@ -1,13 +1,21 @@
 import streamlit as st
-st.set_page_config(page_title='Cats')
-st.header("Types of Cats")
+st.set_page_config(page_title='Pulihora')
+st.header("Types of Pulihora")
 
-col1, col2 = st.columns(2)
+col1, col2, col3,col4 = st.columns(4)
 with col1:
-  st.subheader("Persian Cat")
-  st.image("./pilli1.jpg", caption="Persian Cat", width=300,use_column_width=True)
-  st.write("Persian cats are cute")
+  st.subheader("Nimakai pulihora")
+  st.image("./lemon.jpg", caption="Persian Cat", width=300,use_column_width=True)
+  st.write("Made with Nimakai")
 with col2:
-  st.subheader("Ragdoll Cat")
-  st.image("./pilli2.jpg", caption="Ragdoll Cat", width=300,use_column_width=True)
-  st.write("Ragdoll cats are proud")
+  st.subheader("chinthapandu pulihora")
+  st.image("./tamarind.jpg", caption="Ragdoll Cat", width=300,use_column_width=True)
+  st.write("Made with chinthakai")
+  with col3:
+  st.subheader("thiruapathi Pulihora")
+  st.image("./thirupathi.jpg", caption="Persian Cat", width=300,use_column_width=True)
+  st.write("Made in thirupathi")
+with col4:
+  st.subheader("krishna Pulihora ")
+  st.image("./krishna.jpg", caption="Ragdoll Cat", width=300,use_column_width=True)
+  st.write("Made by Pulihora")
