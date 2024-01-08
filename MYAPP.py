2a5dccfa1401cefae81fea33763cb6dad16c0c75
Patcha Hemanth Lakshman <137917960+HPL1570@users.noreply.github.com>
2024-01-08 12:29:29 +0530

    Update MYAPP.py

--- MYAPP.py
+++ MYAPP.py
@@ -5,17 +5,17 @@ st.header("Types of Pulihora")
 col1, col2, col3,col4 = st.columns(4)
 with col1:
   st.subheader("Nimakai pulihora")
-  st.image("./lemon.jpeg", caption="Persian Cat", width=300,use_column_width=True)
+  st.image("./lemon.jpeg", caption="Nimakai Pulihora", width=300,use_column_width=True)
   st.write("Made with Nimakai")
 with col2:
   st.subheader("chinthapandu pulihora")
-  st.image("./tamarind.jpeg", caption="Ragdoll Cat", width=300,use_column_width=True)
+  st.image("./tamarind.jpeg", caption="chinthapandu pulihora", width=300,use_column_width=True)
   st.write("Made with chinthakai")
 with col3:
   st.subheader("thiruapathi Pulihora")
-  st.image("./thirupathi.jpg", caption="Persian Cat", width=300,use_column_width=True)
+  st.image("./thirupathi.jpg", caption="Thiruapathi Pulihora", width=300,use_column_width=True)
   st.write("Made in thirupathi")
 with col4:
-  st.subheader("krishna Pulihora ")
-  st.image("./krishna.png", caption="Ragdoll Cat", width=300,use_column_width=True)
+  st.subheader("Pulihora ")
+  st.image("./krishna.png", caption="Pulihora", width=300,use_column_width=True)
   st.write("Made by Pulihora")
