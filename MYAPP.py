e2db622f2cbd144f99036add89addf23f5599f4f
Patcha Hemanth Lakshman <137917960+HPL1570@users.noreply.github.com>
2024-01-08 12:18:02 +0530

    Update MYAPP.py

--- MYAPP.py
+++ MYAPP.py
@@ -5,11 +5,11 @@ st.header("Types of Pulihora")
 col1, col2, col3,col4 = st.columns(4)
 with col1:
   st.subheader("Nimakai pulihora")
-  st.image("./lemon.jpg", caption="Persian Cat", width=300,use_column_width=True)
+  st.image("./lemon.jpeg", caption="Persian Cat", width=300,use_column_width=True)
   st.write("Made with Nimakai")
 with col2:
   st.subheader("chinthapandu pulihora")
-  st.image("./tamarind.jpg", caption="Ragdoll Cat", width=300,use_column_width=True)
+  st.image("./tamarind.jpeg", caption="Ragdoll Cat", width=300,use_column_width=True)
   st.write("Made with chinthakai")
   with col3:
   st.subheader("thiruapathi Pulihora")
